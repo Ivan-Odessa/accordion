fa19cfcd0971c032e07440b68f49306029b9a601
Ivan-Odessa <Ajluctan@ukr.net>
2023-02-06 22:49:53 +0200

    Added app accordion

---
+++ js/app.js
@@ -0,0 +1,33 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    const itemsTitle = document.querySelectorAll("[data-item-title]");
+    itemsTitle.forEach((item => {
+        item.addEventListener("click", (function() {
+            this.nextElementSibling.classList.toggle("item-accordion__content--hidden");
+        }));
+    }));
+    window["FLS"] = true;
+    isWebp();
+})();
